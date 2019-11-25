c91f50d1bf36a4072605aac52c5743f2e408c5c8
Đạt <vovandat28051999@gmail.com>
2019-11-26 02:49:40 +0700

    done b1

--- Bai1.py
+++ Bai1.py
@@ -1,4 +1,5 @@
 import copy
+import operator
 
 class Literal():
     def __init__(self, liter):
@@ -7,9 +8,16 @@ class Literal():
     
     def __eq__(self, value):
         return self.char == value.char and self.isNeg == value.isNeg
+    
+    def __hash__(self):
+        return hash(('char', self.char,
+                 'isNeg', self.isNeg))
+    
+    def __lt__(self, value):
+        return self.char < value.char
 
     def getNegative(self):
-        l = copy.copy(self)
+        l = copy.deepcopy(self)
         l.char = self.char
         l.isNeg = not(self.isNeg)
         return l
@@ -23,16 +31,28 @@ class Clause:
         self.liters = [Literal(l) for l in [ l.strip() for l in liters.split("OR")]]
 
     def __eq__(self, value):
-        for i in range(0,len(value.liters)):
-            if self.liters[i]!=value.liters[i]:
-                return False
-        return True
+        opSelf = self.getOptimize()
+        opValue = value.getOptimize()
+        if(len(opSelf.liters)==len(opValue.liters)):
+            for l in opValue.liters:
+                if l not in opSelf.liters:
+                    return False
+            return True
+        return False
 
     def __str__(self):
-        result = ""
-        for i in range(0,len(self.liters) - 1):
-            result += str(self.liters[i]) + " OR "
-        result += str(self.liters[len(self.liters)-1])
+        opClause = self.getOptimize()
+        if opClause.liters:
+            result = ""
+            for i in range(0,len(opClause.liters) - 1):
+                result += str(opClause.liters[i]) + " OR "
+            result += str(opClause.liters[len(opClause.liters)-1])
+            return result
+        return "{}"
+
+    def __add__(self, value):
+        result = copy.deepcopy(self)
+        result.liters += value.liters
         return result
 
     def getNegative(self):
@@ -40,29 +60,81 @@ class Clause:
         for l in self.liters:
           res.append(l.getNegative())
         return res
+    
+    def getOptimize(self):
+        result = copy.deepcopy(self)
+        for l in result.liters:
+            if l.getNegative() in result.liters:
+                result.liters.remove(l)
+                result.liters.remove(l.getNegative())
+        result.liters = list(set(result.liters))
+        result.liters.sort()
+        return result
+    
+    def isTrueClause(self):
+        for l in self.liters:
+            if l.getNegative() in self.liters:
+                return True
+        False
 
 def PLResolve(c1,c2):
-    
+    result = list()
+    for i in range(len(c2.liters)):
+        c1Ins = copy.deepcopy(c1)
+        c2Ins = copy.deepcopy(c2)
+        if c2.liters[i].getNegative() in c1.liters:
+            c1Ins.liters.remove(c2.liters[i].getNegative())
+            c2Ins.liters.remove(c2.liters[i])
+            res = c1Ins + c2Ins
+            if res not in result:
+                result.append(res)
+    return result
 
-def  PLResolution(kb,a):
-    clauses = copy.copy(kb)
+def  PLResolution(kb,a,fOut):
+    clauses = copy.deepcopy(kb)
     for literal in a.getNegative():
-        clauses.append(literal.getNegative())
+        clauses.append(Clause(str(literal)))
     new = list()
-    return True
+    while(True):
+        clauseGenerate = list()
+        for i in range(len(clauses)-1):
+            for j in range(i + 1, len(clauses)):
+                resolvents = PLResolve(clauses[i],clauses[j])
+                for re in resolvents:
+                    if not(re.isTrueClause()) and\
+                            re.getOptimize() not in new and\
+                            re.getOptimize() not in clauseGenerate and\
+                            re.getOptimize() not in clauses:
+                        clauseGenerate.append(re.getOptimize())
+        #write file here
+        fOut.write(str(len(clauseGenerate))+'\n')
+        # print(len(clauseGenerate))
+        for i in clauseGenerate:
+            fOut.write(str(i)+'\n')
+            # print(str(i))
+        for i in clauseGenerate:
+            if str(i)=="{}":
+                fOut.write("YES\n")
+                return True
+        new += clauseGenerate
+        if all(elem in clauses  for elem in new):
+            fOut.write("NO\n")
+            return False
+        for n in new:
+            if n not in clauses:
+                clauses.append(n)
+    return False
 
 def main():
     #<data handler>
     a = ""
     kb = list()
-    with open("Input.txt") as f:
+    with open("input_1.txt") as f:
         lineList = f.readlines()
     a = Clause(lineList[0].strip())
     for i in range(int(lineList[1])):
         kb.append(Clause(lineList[i+2].strip()))
- 
-    result = PLResolution(kb,a)
-    
     #<data handler/>
-
+    fOut = open("output.txt", "w")
+    PLResolution(kb,a,fOut)
 main()
